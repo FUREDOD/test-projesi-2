117c7a62f5a91048bef581f65861acfa28e0fe13
furkan <furkan.ede1@gmail.com>
2023-05-01 21:19:06 +0300

    test projesi2

---
+++ test-projesi-2/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/DefineX.jpeg" />
+    <HelloWorld msg="test proje" />
+    <HomeView />
+  </div>
+</template>
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+import AboutView from "./AboutView.vue";
+import HomeView from "./HomeView.vue";
+import login from "./login.vue";
+import UserRegistration from "./UserRegistration.vue";
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+    AboutView,
+    HomeView,
+    login,
+    UserRegistration
+  },
+};
+</script>
---
+++ test-projesi-2/src/views/src/router.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import Register from "./views/Register.vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
+    },
+  ],
+});
